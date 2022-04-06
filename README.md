4bec38e656925b325fa765017eee7b6c792a5ceb
Sarah Gilbreath <thenoiseinspace@gmail.com>
2022-04-05 22:55:41 -0400

    pushing up updated js

--- README.md
+++ README.md
@@ -3,10 +3,53 @@
 This is a weather forcasting app that allows the user to select a city and recieve both current weather and a five-day forcast. 
 
 Deployment links:
+GitHub repo link: https://github.com/thenoiseinspace/weather-app
+
+Live deployment link: https://thenoiseinspace.github.io/weather-app
 
 Image (also in assets folder):
 
 Acknowledgements: 
 
-Study group: Lina Choi, James Edwards, Anthony Chang
+Study group: Lina Choi, James Edwards, Anthony Chang, Brian Samuels, Noah Brunner
 Jonah Lawrence, Dev Pro Tips: https://www.youtube.com/watch?v=WZNG8UomjSI 
+
+
+
+///////////////////////////
+# 06 Server-Side APIs: Weather Dashboard
+
+Use the [OpenWeather One Call API](https://openweathermap.org/api/one-call-api) to retrieve weather data for cities. Read through the documentation for setup and usage instructions. You will use `localStorage` to store any persistent data. For more information on how to work with the OpenWeather API, refer to the [Full-Stack Blog on how to use API keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys).
+
+
+```
+
+## Acceptance Criteria
+
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN I view the UV index
+THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+```
+
+## Mock-Up
+
+The following image shows the web application's appearance and functionality:
+
+![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./Assets/06-server-side-apis-homework-demo.png)
+
+### Technical Acceptance Criteria: 40%
+
+* Satisfies all of the above acceptance criteria plus the following:
+
+    * Uses the OpenWeather API to retrieve weather data.
+
+    * Uses `localStorage` to store persistent data.
--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,86 @@
-let weather ={
-    "apiKey": "7580a995f9e494f6242cd7f55176487d"
-    fetchWeather: function(){
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=7580a995f9e494f6242cd7f55176487d"
-        ).then((response) => response.json())
-        .then((data) => console.log(data))
-    }
-}; 
+//Setting up the API key and the initial city
+var apiKey = "7580a995f9e494f6242cd7f55176487d"; 
+//var city = document.getElementById("search-city");
 
-//Note - fetchWeather sbove is not stated properly i think? 
-////Note - need to update longitude and latitude in the url
+//Setting up moment to get the dates
+var today = moment(); 
+$('.current-date').text(today.format('L'));
 
-function fiveDayForecast(lat, long){
-    fetch("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=7580a995f9e494f6242cd7f55176487d" + apiKey)
-}
+//Setting up the element for the button
+var saveButonEl = $('.search-button')
 
-function getWeatherInfo(){
-    var city = $("#city-search").val(); 
-    fetch("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=7580a995f9e494f6242cd7f55176487d" + city + "&units-impereial&appid-" + apikey)
-    .then(function (response){
-        return response.json(); 
-    })
-    .then(function (data){
-        console.log(data);
-        fiveDayForecast; 
-    }); 
-}
+//This is the function to retrieve the current weather at the chosen city and also retrieves the lat and log
+function getWeatherData(){
+    var city = $('.search-city').val(); 
+    var requestURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&units=imperial&appid=" + apiKey
 
-$(#"search-btn").on('click', getWeatherInfo); 
-//check the button's actual id, i don;t think this is it
+    console.log(requestURL); //adding these at every single step to see if it's working
+    
+    //two fetch requests for same function
+    fetch(requestURL).then(function(response){
+            console.log("the first fetch is connected") //no literally every step
+            console.log(response)
+            return response.json() 
+        }).then(function(sanitizedData){  //calling the json-ified data "sanitized"
+            console.log(sanitizedData);
+       
+            var lat = sanitizedData.coord.lat;
+            var lon = sanitizedData.coord.lon; 
+            console.log(lat); 
+            console.log(lon); 
+
+                        //set these to text content, not variables 
+                        //going through the nodes of the dom to find location
+                    var cityName = sanitizedData.name;
+                    var temperature = sanitizedData.main.temp; 
+                        console.log(temperature); 
+                    var humidity = sanitizedData.main.humidity; 
+                        console.log(humidity);
+                    var wind = JSON.stringify(sanitizedData.wind.speed); 
+                        console.log("this is wind" + wind);
+                    var icon = data.weather[0].icon; 
+                    var todaysWeather = [cityName, temperature, humidity, wind, icon, lat, lon]
+
+                    console.log(todaysWeather); 
+
+                    //appending all of the values we just got into the text on the page
+                    //Thank you to Travis for walking me through this part in office hours
+                    document.querySelector('.city').innerText = cityName; 
+                    document.querySelector('.main-temp').innerText = temperature + "degrees"; 
+                    document.querySelector('.main-wind').innerText = wind + "miles per hour"; 
+                    document.querySelector('.icon').src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+                    
+                    // var uvIndex = sanitizedData.main.uvi; 
+                    //     console.log(uvIndex);
+                    //var weatherIcon = sanitizedData.current.weather[0].icon; 
+                        //console.log(weatherIcon);
+
+            var secondURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=" + apiKey
+
+            //second fetch request
+            //I'm nesting both fetch requests in the same function so they can use the same data
+            fetch(
+                secondURL).then(function(secondFetchResponse){
+                    console.log("the second fetch is connected")
+                    console.log(secondFetchResponse)
+                    return secondFetchResponse.json()
+                 }).then(function(data){
+                     console.log(data)
+
+                    //putting the uv call in this same fetch
+                    //this homework is very nested in on itself. I don't like it. Gotdang Hapsburg family tree over here. 
+
+                    var uvIndex = data.current.uvi; 
+                    console.log(uvIndex);
+
+                    document.querySelector('.uv').innerText = uvIndex; 
+
+
+                 })
+        
+         })
+        }
+ 
+
+//making it all run on click
+saveButonEl.on('click', getWeatherData)
